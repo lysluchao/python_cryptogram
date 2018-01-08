019a415962371696ab77ade1d1603c1a1aa7a81a
luchao <1457065670@qq.com>
2018-01-08 16:23:28 +0800

    加密和签名

--- PycharmProjects/untitled/rsa.py
+++ PycharmProjects/untitled/rsa.py
@@ -1,17 +1,31 @@
-m = 10
-p = 19
-q = 23
-n = p * q
-ola_n = (p - 1) * (q - 1)
-d = 0
-e = 13
-# 求出公钥
-while (d * e) % ola_n != (1 % ola_n):
-    d += 1
-    print(d)
-print("公钥为{0} 明文为{1}".format(d, m))
-# 加密
-c = pow(m, d) % n
-# 解密
-m = pow(c, e) % n
-print("加密后 {0},解密后 {1}".format(c, m))
+class rsa():
+    __m = 8 ; __p = 7;__q = 17;__e = 5
+
+    __n = __p * __q
+    __ola_n = (__p - 1) * (__q - 1)
+    __d = 0
+
+    # 求出公钥
+    while (__d * __e) % __ola_n != (1 % __ola_n):
+        __d += 1
+        print(__d)
+    print("公钥为{0} 明文为{1}".format(__d, __m))
+
+    def crytogram(self):
+        # 加密
+        c = pow(self.__m, self.__d) % self.__n
+        # 解密
+        m = pow(c, self.__e) % self.__n
+        print("加密后 {0},解密后 {1}".format(c, m))
+
+    def signature(self):
+        # 签名
+        s = pow(self.__m.__hash__(), self.__e) % self.__n
+        print("签名结果 m{} s{}".format(self.__m, s))
+        # 验证
+        s1 = pow(s, self.__d) % self.__n
+        print(s1 == self.__m)
+
+
+k = rsa()
+k.signature()
